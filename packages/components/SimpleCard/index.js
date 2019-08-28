ccdc36acf69bd680eed559ea766fb2580521764e
Allison Shultes <allison.shultes@bbc.co.uk>
2019-08-28 16:44:40 +0100

    Adds default props and proptypes

--- packages/components/SimpleCard/index.js
+++ packages/components/SimpleCard/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,80 +7,105 @@ import Button from 'react-bootstrap/Button';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faTrash,
-    faPen
+  faTrash,
+  faPen
 } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap-css-only/css/bootstrap.css';
 
 class SimpleCard extends Component {
-
     handleDelete = () => {
-        const confirmDeleteText = "Click OK if you wish to delete or cancel if you don't";
-        const cancelDelteText = "All is good, it was not deleted";
-        const confirmationPrompt = confirm(confirmDeleteText);
-        
-        if (confirmationPrompt) {
-            this.props.handleDelete ? this.props.handleDelete(this.props.id) : alert(cancelDelteText);
-        }
+      const confirmDeleteText = "Click OK if you wish to delete or cancel if you don't";
+      const cancelDelteText = 'All is good, it was not deleted';
+      const confirmationPrompt = confirm(confirmDeleteText);
+
+      if (confirmationPrompt) {
+        this.props.handleDelete ? this.props.handleDelete(this.props.id) : alert(cancelDelteText);
+      }
     };
 
     handleEdit = () => {
-        this.props.handleEdit(this.props.id);
+      this.props.handleEdit(this.props.id);
     }
 
     showLinkPath = () => {
-        return this.props.showLinkPath(this.props.id) || '';
+      return this.props.showLinkPath(this.props.id) || '';
     }
 
     render() {
 
-        return (
-            <Card style={{ width: '100%', marginBottom: '1em' }}>
-                <Card.Body>
-                    <Row>
-                        <LinkContainer to={this.showLinkPath()} style={{ cursor: 'pointer' }}>
-                            <Col xs={8} sm={10}>
-                                <Card.Title>
-                                    {this.props.title}
-                                </Card.Title>
-                            </Col>
-                        </LinkContainer>
-                        <Col xs={2} sm={1}>
-                            <Card.Link>
-                                <Button
-                                    onClick={this.handleEdit}
-                                    variant="outline-secondary"
-                                    size="sm"
-                                    aria-label="Edit button"
-                                >
-                                    <FontAwesomeIcon icon={faPen} />
-                                </Button>
-                            </Card.Link>
-                        </Col>
-                        <Col xs={2} sm={1}>
-                            <Card.Link>
-                                <Button
-                                    onClick={this.handleDelete}
-                                    variant="outline-secondary"
-                                    size="sm"
-                                    aria-label="Delete button"
-                                >
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </Card.Link>
-                        </Col>
-                    </Row>
-                        <Row>
-                            <Col xs={10} sm={11}>
-                                <Card.Subtitle className="mb-2 text-muted">
-                                    {this.props.description}
-                                </Card.Subtitle>
-                            </Col>
-                        </Row>
-                </Card.Body>
-            </Card>
-        );
+      return (
+        <Card style={ { width: '100%', marginBottom: '1em' } }>
+          <Card.Body>
+            <Row>
+              <LinkContainer to={ this.showLinkPath() } style={ { cursor: 'pointer' } }>
+                <Col xs={ 8 } sm={ 10 }>
+                  <Card.Title>
+                    {this.props.title}
+                  </Card.Title>
+                </Col>
+              </LinkContainer>
+              <Col xs={ 2 } sm={ 1 }>
+                <Card.Link>
+                  <Button
+                    onClick={ this.handleEdit }
+                    variant="outline-secondary"
+                    size="sm"
+                    aria-label="Edit button"
+                  >
+                    <FontAwesomeIcon icon={ faPen } />
+                  </Button>
+                </Card.Link>
+              </Col>
+              <Col xs={ 2 } sm={ 1 }>
+                <Card.Link>
+                  <Button
+                    onClick={ this.handleDelete }
+                    variant="outline-secondary"
+                    size="sm"
+                    aria-label="Delete button"
+                  >
+                    <FontAwesomeIcon icon={ faTrash } />
+                  </Button>
+                </Card.Link>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={ 10 } sm={ 11 }>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {this.props.description}
+                </Card.Subtitle>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      );
     }
-}
+};
+
+SimpleCard.propTypes = {
+  key: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  handleEdit: PropTypes.func,
+  handleDelete: PropTypes.func,
+  showLinkPath: PropTypes.func
+};
+
+SimpleCard.defaultProps = {
+  key: 'key_1234',
+  id: '1234',
+  title: 'Default Title String',
+  description: 'This is a default description string',
+  handleEdit: () => {
+    console.log('Edit button clicked');
+  },
+  handleDelete: () => {
+    console.log('Delete button clicked');
+  },
+  showLinkPath: () => {
+    console.log('Card clicked');
+  }
+};
 
 export default SimpleCard;
--- packages/components/SimpleCard/stories/index.stories.js
+++ packages/components/SimpleCard/stories/index.stories.js
@@ -6,26 +6,26 @@ import StoryRouter from 'storybook-react-router';
 import SimpleCard from '../index.js';
 
 export const item = {
-    id: '1234',
-    key: 'abc123',
-    title: 'Sample Simple Card Title',
-    description: 'This is a sample card description. This is fun!',
+  id: '1234',
+  key: 'abc123',
+  title: 'Sample Simple Card Title',
+  description: 'This is a sample card description. This is fun!',
 };
 
 const cardActions = actions({ handleEdit: 'Edit button clicked', handleDelete: 'Delete button clicked', showLinkPath: 'Card clicked' });
 
 storiesOf('Simple Card', module)
-    .addDecorator(StoryRouter())
-    .add('Default', () => {
-        return (
-            <section style={{ height: '75vh', overflow: 'scroll' }}>
-                <SimpleCard
-                    key={item.key}
-                    id={item.id}
-                    title={item.title}
-                    description={item.description}
-                    {...cardActions}
-                />
-            </section>
-        )
-    })
+  .addDecorator(StoryRouter())
+  .add('Default', () => {
+    return (
+      <section style={ { height: '75vh', overflow: 'scroll' } }>
+        <SimpleCard
+          key={ item.key }
+          id={ item.id }
+          title={ item.title }
+          description={ item.description }
+          { ...cardActions }
+        />
+      </section>
+    );
+  });
