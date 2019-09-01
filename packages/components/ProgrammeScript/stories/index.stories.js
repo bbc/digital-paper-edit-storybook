60bca49f05df554c22bfad54b8d6ca2cbdc4eb2b
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-09-01 10:35:03 +0100

    Refactoring some more on the Sortable Elements

---
+++ packages/components/ProgrammeScript/stories/index.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { actions } from '@storybook/addon-actions';
+import ProgrammeScript from '../index.js';
+
+export const handleReorderActions = actions({ handleReorder: 'Handle reorder' });
+export const handleDeleteActions = actions({ handleDelete: 'Handle delete' });
+export const handleEditActions = actions({ handleEdit: 'Handle edit' });
+
+export const items = [ {
+  id: '1234',
+  start: '',
+  end: 'abc123',
+  text: 'Sample Simple Card Title One',
+  type: 'This is a sample card description. This is fun!',
+}, { ...item,
+  id: '5678',
+  key: 'def456',
+  title: 'Sample Simple Card Title Two',
+  description: 'This is a sample card description. This is fun!',
+  display: true,
+  url: '/projects/1/transcripts/5678'
+} ];
+
+storiesOf('ProgrammeScript', module)
+  .add('Default', () =>
+    <ProgrammeScript
+      elements={ items }
+      handleReorder={ handleReorderActions }
+      handleDelete={ handleDeleteActions }
+      handleEdit={ handleEditActions }
+    />
+  );
