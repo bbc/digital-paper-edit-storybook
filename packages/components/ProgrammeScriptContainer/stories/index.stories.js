c2769b17cbb3d29c55c93e9dc959d4ca985d1591
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-09-06 11:58:44 +0100

    Updating stories for programme script

--- packages/components/ProgrammeScriptContainer/stories/index.stories.js
+++ packages/components/ProgrammeScriptContainer/stories/index.stories.js
@@ -9,21 +9,7 @@ export const handleDeleteActions = actions({ handleDelete: 'Handle delete' });
 export const handleEditActions = actions({ handleEdit: 'Handle edit' });
 
 export const items = [
-  {
-    id: '1234',
-    start: '',
-    end: 'abc123',
-    text: 'Sample Simple Card Title One',
-    type: 'This is a sample card description. This is fun!',
-  },
-  { ...item,
-    id: '5678',
-    key: 'def456',
-    title: 'Sample Simple Card Title Two',
-    description: 'This is a sample card description. This is fun!',
-    display: true,
-    url: '/projects/1/transcripts/5678'
-  }
+
 ];
 
 storiesOf('ProgrammeScript', module)
