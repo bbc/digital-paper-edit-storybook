4878a07ad1e535cda0dd8365b519232986c9382e
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-09-06 12:55:36 +0100

    Added babel config - which allows for babel-jest to pick up presets to transpile JS files

--- packages/components/SimpleCard/index.test.js
+++ packages/components/SimpleCard/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SimpleCard from './SimpleCard';
+import SimpleCard from './index.js';
 
 describe('VideoContextProgressBar', () => {
   const mockVideoContext = {};
