bde824cc396ee44020e7b200b1ca5913778350c6
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-09-11 11:16:13 +0100

    refactor voice over

--- packages/components/ProgrammeScriptContainer/ProgrammeElements/VoiceOver.js
+++ packages/components/ProgrammeScriptContainer/ProgrammeElements/VoiceOver.js
@@ -1,24 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophoneAlt } from '@fortawesome/free-solid-svg-icons';
 
-class VoiceOver extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <p className={ 'text-muted' }>
-          <FontAwesomeIcon icon={ faMicrophoneAlt } /> { this.props.text }
-        </p>
-      </>
-    );
-  }
-}
+const VoiceOver = ({ text }) =>
+  <p className={ 'text-muted' }>
+    <FontAwesomeIcon icon={ faMicrophoneAlt } />
+    { text }
+  </p>;
 
 export default VoiceOver;
