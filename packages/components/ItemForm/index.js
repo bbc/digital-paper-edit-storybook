5773037d143049b9e7455d6216c6e48c57c68fa3
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-09-13 09:06:38 +0100

    Small refactor that was left over

--- packages/components/ItemForm/index.js
+++ packages/components/ItemForm/index.js
@@ -7,9 +7,7 @@ import Modal from 'react-bootstrap/Modal';
 const ItemForm = (props) => {
 
   const [ description, setDescription ] = useState(props.description);
-
   const [ isValidated, setIsValidated ] = useState(false);
-
   const [ title, setTitle ] = useState(props.title);
 
   const handleSubmit = (event) => {
@@ -18,11 +16,9 @@ const ItemForm = (props) => {
     event.stopPropagation();
 
     const formIsValid = form.checkValidity();
+    setIsValidated(true);
 
-    if (!formIsValid) {
-      setIsValidated(true);
-    } else if (formIsValid) {
-      setIsValidated(true);
+    if (formIsValid) {
       const editedProject = {
         title: title,
         description: description,
@@ -35,7 +31,8 @@ const ItemForm = (props) => {
 
   return (
 
-    <Form noValidate validated={ isValidated }
+    <Form noValidate
+      validated={ isValidated }
       onSubmit={ handleSubmit }
     >
       <Form.Group controlId="formBasicEmail">
@@ -84,17 +81,14 @@ ItemForm.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string,
   description: PropTypes.string,
-  isNewItemModalShow: PropTypes.bool.isRequired,
-  modalTitle: PropTypes.string.isRequired,
+  showModal: PropTypes.bool,
+  modalTitle: PropTypes.string,
   handleSaveForm: PropTypes.func.isRequired,
 };
 
 ItemForm.defaultProps = {
-  handleSaveForm: () => {
-    console.log('Handling a project save');
-  },
   id: 1,
-  isNewItemModalShow: false,
+  showModal: false,
   modalTitle: 'New Project',
 };
 
