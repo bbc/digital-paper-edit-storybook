982da43f0b2f71ec0dc2cc1090a0f3378dbf3888
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-10-18 15:28:48 +0100

    Updates

--- vendors~main.3e17c7954f63630e0252.bundle.js
+++ vendors~main.3e17c7954f63630e0252.bundle.js
@@ -433,4 +433,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.1f96e06542ab9d0b2b20.bundle.js.map
+//# sourceMappingURL=vendors~main.3e17c7954f63630e0252.bundle.js.map
