13c5493157bfd66010027882359bd2e11b809d07
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-10-21 15:46:02 +0100

    Adding knob for urls

--- packages/components/PreviewCanvas/stories/index.stories.js
+++ packages/components/PreviewCanvas/stories/index.stories.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import PreviewCanvas from '../index.js';
 
-const playlist = [
-  { type:'video', start:0, sourceStart: 30, duration:10, src:'https://download.ted.com/talks/MorganVague_2018X.mp4' },
-  { type:'video', start:10, sourceStart: 40, duration:10, src:'https://download.ted.com/talks/IvanPoupyrev_2019.mp4' },
-  { type:'video', start:20, sourceStart: 50, duration:10, src:'https://download.ted.com/talks/KateDarling_2018S-950k.mp4' },
+const stories = storiesOf('PreviewCanvas', module);
+stories.addDecorator(withKnobs);
 
-];
+stories.add('VideoContext', () => {
 
-storiesOf('PreviewCanvas', module)
-  .add('VideoContext', () =>
-    <PreviewCanvas
-      playlist={ playlist }
-    />
-  );
+  const defaultValue = [
+    { type:'video', start:0, sourceStart: 30, duration:10, src:'https://download.ted.com/talks/MorganVague_2018X.mp4' },
+    { type:'video', start:10, sourceStart: 40, duration:10, src:'https://download.ted.com/talks/IvanPoupyrev_2019.mp4' },
+    { type:'video', start:20, sourceStart: 50, duration:10, src:'https://download.ted.com/talks/KateDarling_2018S-950k.mp4' },
+  ];
+
+  return (<PreviewCanvas
+    playlist={ object('playlist', defaultValue) }
+  />);
+});
