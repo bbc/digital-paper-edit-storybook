ca49fbaff0ddb306c320d6a0acb9bd4be387e33a
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-01-28 14:12:24 -0500

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -63,12 +63,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: path.resolve(__dirname, 'src'),
         // TODO: because it uses entry point to determine graph of dependencies, might not be needed to exclude test ans sample files?
-        exclude: /(node_modules|bower_components|build|dist|demo|.storybook|storybook-static)/,
+        exclude: /(node_modules|bower_components|build|dist|demo|storybook-static)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ '@babel/preset-env', '@babel/preset-react' ]
-          }
+          loader: 'babel-loader'
         }
       }
     ]
