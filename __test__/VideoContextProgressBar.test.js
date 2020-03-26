aab9535c2055fcb5f4d24a976e417b30a5c154c7
Eimi Okuno <emettely@users.noreply.github.com>
2020-03-26 15:04:06 +0900

    i45, related to #45 (#46)
    * Adding travis YML and updating package.json with public Config
    
    * Removing options that package.json could not understand
    
    * Moving to react-transcipt-testing library, incomplete implementation but it'll do.
    
    * Removing snaps
    
    * Ignoring test results
    
    * 1.5.0
    
    * Different api key
    
    * Removing extra gh page release step
    
    * Updated readme with the travis deployment details
    
    * Travis to have installation step
    
    * Adding global deps
    
    * Updated readme with the travis deployment details
    
    * Cleaning up tests
    
    * Moved test to standard location

---
+++ __test__/VideoContextProgressBar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideoContextProgressBar from '../src/PreviewCanvas/VideoContextPreview';
+
+describe('VideoContextProgressBar', () => {
+  it('should render', () => {
+    // tbd
+  });
+});
