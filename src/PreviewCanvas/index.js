6b0a60ff4c026fd9ac2199b29cee4c1e9933c54b
Allison Shultes <allison.shultes@bbc.co.uk>
2020-04-21 11:34:38 -0400

    [FIX]: Revert the revert of the local import of PC

--- src/PreviewCanvas/index.js
+++ src/PreviewCanvas/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useRef } from 'react';
-import VideoContextPreview from '@bbc/digital-paper-edit-storybook/PreviewCanvas';
+import VideoContextPreview from './VideoContextPreview';
 import PropTypes from 'prop-types';
 
 const PreviewCanvas = props => {
