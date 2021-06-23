a483b5a6dc0a93723f4bd835e66780251a764666
sarahrainbow <sarah@thebatesons.com>
2021-06-23 16:13:21 +0100

    initial analytics

--- src/TranscriptRow/index.js
+++ src/TranscriptRow/index.js
@@ -155,6 +155,7 @@ const TranscriptRow = (props) => {
           <LinkContainer
             to={ props.url }
             style={ { color: '#007bff', cursor: 'pointer' } }
+            onClick={ props.handleClick }
           >
             <p style={ {
               display: 'flex'
@@ -226,6 +227,7 @@ TranscriptRow.propTypes = {
   transcriptionDuration: PropTypes.number,
   handleDeleteItem: PropTypes.func.isRequired,
   handleEditItem: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
   icon: PropTypes.any,
   id: PropTypes.string.isRequired,
   status: PropTypes.string,
